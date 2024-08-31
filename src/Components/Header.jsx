d16fd204c51ce8fc4b59fcc494e9484152045097
simonkadili47 <simonkadili47@gmail.com>
2024-08-31 09:07:11 +0100

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,20 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex ml-10 mt-8 text-black bg-white font-bold">
-      <div className="flex justify-start items-start">
+    <div className="flex items-center justify-between ml-10 mt-8 text-black bg-white font-bold">
+      {/* Left section with text */}
+      <div className="flex items-center">
         <div className="text-xl">
           <h1>Lyf</h1>
         </div>
         <div className="ml-3">
           <h1>/insuarance@tiropay.au</h1>
         </div>
-        <div className="ml-4 border px-4 py-2 rounded-full">
+      </div>
+      
+      {/* Right section with buttons */}
+      <div className="flex space-x-4 mr-24">
+        <div className="border px-4 py-2 rounded-full hover:bg-gray-200 hover:border-gray-400">
           <button>
-            <a href="">Login</a>
+            <a href="#">Login</a>
           </button>
         </div>
-        <div className="ml-4 border px-4 py-2 rounded-full bg-black text-white">
+        <div className="border px-4 py-2 rounded-full bg-black text-white">
           <button>Get Started</button>
         </div>
       </div>
