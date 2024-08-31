9391df06618bed8e4bc6a32484553fcad3bd844b
simonkadili47 <simonkadili47@gmail.com>
2024-08-31 09:15:15 +0100

    body

---
+++ src/Components/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Body = () => {
+  return (
+    <div className="flex flex-col items-center text-black pt-0 mt-10">
+      <div className="font-bold text-3xl text-center">
+        <p>Life Planning, Making</p>
+        <p>Easy to Turn Dreams a Reality.</p>
+      </div>
+      <div className="text-sm mt-4 text-center">
+        <p>Get exclusive offers on purchase of any plans</p>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Components/Header'
+import Body from './Components/Body'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <Body/>
+
 
     </div>
   )
