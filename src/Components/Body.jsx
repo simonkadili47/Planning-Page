6c1f153233f23e4b03e7cd8b41a86abbf1cf20e4
simonkadili47 <simonkadili47@gmail.com>
2024-09-02 07:55:57 +0100

    changes in mobile responsive

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -51,9 +51,9 @@ const Body = () => {
   }, [index, isTyping, animationState]);
 
   return (
-    <div className="flex flex-col items-center text-black pt-0 mt-6">
+    <div className="flex flex-col items-center mt-3 text-black py-4 sm:py-6">
       {/* Container for typing effect text */}
-      <div className="font-bold text-3xl text-center w-full max-w-xl relative">
+      <div className="font-bold text-2xl sm:text-3xl text-center w-full max-w-xl relative">
         <p className={`absolute inset-0 flex items-center justify-center ${animationState}`}>
           {text}
         </p>
@@ -61,10 +61,10 @@ const Body = () => {
 
       {/* Container for static paragraphs with animation */}
       <div className="relative mt-4 w-full max-w-xl text-center">
-        <p className={`text-3xl font-bold text-black animate-bounce`}>
+        <p className={`text-2xl sm:text-3xl font-bold text-black animate-bounce`}>
           Easy to Turn Dreams a Reality.
         </p>
-        <p className={`text-sm mt-2 animate-fadeInOut`}>
+        <p className={`text-sm sm:text-base mt-2 animate-fadeInOut`}>
           Get exclusive offers on purchase of any plans
         </p>
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between ml-10 mt-8 text-black bg-white font-bold">
+    <div className="flex flex-col sm:flex-row items-center justify-between ml-4 sm:ml-10 mt-4 sm:mt-8 text-black bg-white font-bold">
       {/* Left section with text */}
       <div className="flex items-center">
-        <div className="text-xl">
+        <div className="text-lg sm:text-xl">
           <h1>Lyf</h1>
         </div>
-        <div className="ml-3">
+        <div className="ml-1 sm:ml-3 text-sm sm:text-base">
           <h1>/insuarance@tiropay.au</h1>
         </div>
       </div>
       
       {/* Right section with buttons */}
-      <div className="flex space-x-4 mr-24">
-        <div className="border px-4 py-2 rounded-full hover:bg-gray-200 hover:border-gray-400">
+      <div className="flex space-x-2 sm:space-x-4 mt-4 sm:mt-0 mr-4 sm:mr-24">
+        <div className="border px-3 sm:px-4 py-1 sm:py-2 rounded-full hover:bg-gray-200 hover:border-gray-400">
           <button>
             <a href="#">Log In</a>
           </button>
         </div>
-        <div className="border px-4 py-2 rounded-full hover:bg-gray-400 hover:border-customGreen bg-black text-white">
+        <div className="border px-3 sm:px-4 py-1 sm:py-2 rounded-full hover:bg-gray-400 hover:border-customGreen bg-black text-white">
           <button>Get Started</button>
         </div>
       </div>
