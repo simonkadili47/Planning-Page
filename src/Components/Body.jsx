088b3138a1177c11215fedd407804565c670c789
simonkadili47 <simonkadili47@gmail.com>
2024-09-02 11:42:28 +0100

    changes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import 'animate.css'; // Import Animate.css if you installed via npm
+import 'animate.css'; 
 
 const Body = () => {
   const fullText = "Life Planning, Making";
 
   return (
     <div className="flex flex-col items-center mt-3 text-black py-4 sm:py-6">
-      {/* Display the full text directly with animation */}
+   
       <div className="font-bold text-2xl sm:text-3xl text-center w-full max-w-xl animate__animated animate__fadeInDown">
         <p className="flex items-center justify-center">
           {fullText}
         </p>
       </div>
 
-      {/* Container for static paragraphs with animation */}
+     
       <div className="mt-2 w-full max-w-xl text-center">
         <p className="text-2xl sm:text-3xl font-bold text-black animate__animated animate__fadeInDown">
           Easy to Turn Dreams a Reality.
